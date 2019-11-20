655220b0303ef281422b9a8ec16c58c6225c1034
leandro <leandro@3upsolucoes.com.br>
2019-11-20 15:25:39 -0300

    Welcome Screen Style

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import './config/ReactotronConfig';
+
+import React from 'react';
+import { View, Text } from 'react-native';
+import Routes from './routes';
+// import {
+//   Header,
+//   LearnMoreLinks,
+//   Colors,
+//   DebugInstructions,
+//   ReloadInstructions,
+// } from 'react-native/Libraries/NewAppScreen';
+const App = () => <Routes />;
+
+export default App;
